144498604d7aed7ba6b94f3e721b5f38b1d068d7
Romain Pouclet <romain.pouclet@gmail.com>
2018-06-20 09:44:44 -0700

    Add a custom build step to download the video as part of the build process

---
+++ buddybuild_postclone.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Downloading video file..."
+
+curl -# -o BigBunnySampleApp/bigbunny.mp4 http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_stereo_abl.mp4
