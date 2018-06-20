342398393464e5fa7d5d4a8603ef968d0123a798
Romain Pouclet <romain.pouclet@gmail.com>
2018-06-20 12:47:21 -0700

    Create a bunch of sounds to increase the size of the archive

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -2,4 +2,13 @@
 
 echo "Downloading video file..."
 
-curl -# -o BigBunnySampleApp/bigbunny.mp4 http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_stereo_abl.mp4
+#curl -# -o BigBunnySampleApp/bigbunny.mp4 http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_stereo_abl.mp4
+
+echo "Adding a bunch of sounds"
+
+for (( index = 1; index <= 50; index++))
+do
+  cp BigBunnySampleApp/Sounds/soundtrack.mp3 "BigBunnySampleApp/Sounds/soundtrack-copy-$index.mp3"
+done
+
+ls -hal BigBunnySampleApp/Sounds
