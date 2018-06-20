5272f0932cb3f83b5a7006d827d08a4ce7d7d480
Romain Pouclet <romain.pouclet@gmail.com>
2018-06-20 12:51:54 -0700

    Uncomment `curl` command

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading video file..."
 
-#curl -# -o BigBunnySampleApp/bigbunny.mp4 http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_stereo_abl.mp4
+curl -# -o BigBunnySampleApp/bigbunny.mp4 http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_stereo_abl.mp4
 
 echo "Adding a bunch of sounds"
 
